bcb081ca805ce8828b38e2c7c63da3a6c2d29366
Vee Uye <vee.uye@gmail.com>
2022-04-19 15:12:44 +0100

    refactor add endpoint in numbers-routes

--- src/routes/numbers-routes.js
+++ src/routes/numbers-routes.js
@@ -2,23 +2,14 @@ const req = require('express/lib/request');
 
 const router = require('express').Router();
 const { add } = require('../lib/numbers');
-// const res = require('express/lib/response');
-
-// router.get('/add/:a/and/:b', (req, res) => {
-//   const a = parseInt(req.params.a); // eslint-disable-line radix
-//   const b = parseInt(req.params.b); // eslint-disable-line radix
-//   res.status(200).json({ result: add(a, b) });
-// });
 
 router.get('/add/:a/and/:b', (req, res) => {
-  const a = parseInt(req.params.a); // eslint-disable-line radix
-  const b = parseInt(req.params.b); // eslint-disable-line radix
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
 
-  if (Number.isNaN(a) || Number.isNaN(b)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: add(a, b) });
-  }
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).json({ result: add(a, b) });
 });
 
 module.exports = router;
