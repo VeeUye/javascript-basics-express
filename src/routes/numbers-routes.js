87ca04272ef08612a1ab2530e6feab52ed913dc9
Vee Uye <vee.uye@gmail.com>
2022-04-19 15:09:51 +0100

    add error message and 400 status code to add endpoint

--- src/routes/numbers-routes.js
+++ src/routes/numbers-routes.js
@@ -4,10 +4,21 @@ const router = require('express').Router();
 const { add } = require('../lib/numbers');
 // const res = require('express/lib/response');
 
+// router.get('/add/:a/and/:b', (req, res) => {
+//   const a = parseInt(req.params.a); // eslint-disable-line radix
+//   const b = parseInt(req.params.b); // eslint-disable-line radix
+//   res.status(200).json({ result: add(a, b) });
+// });
+
 router.get('/add/:a/and/:b', (req, res) => {
   const a = parseInt(req.params.a); // eslint-disable-line radix
   const b = parseInt(req.params.b); // eslint-disable-line radix
-  res.status(200).json({ result: add(a, b) });
+
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: add(a, b) });
+  }
 });
 
 module.exports = router;
