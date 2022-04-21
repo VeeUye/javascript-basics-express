62baf6befad99cdbccd6123e339ae0140dbae084
Vee Uye <vee.uye@gmail.com>
2022-04-21 23:04:41 +0100

    solve booleans kata

--- src/routes/booleans-routes.js
+++ src/routes/booleans-routes.js
@@ -1,4 +1,36 @@
 const router = require('express').Router();
-const {} = require('../lib/numbers');
+const { negate, truthiness, isOdd, startsWith } = require('../lib/booleans');
+
+router.post('/negate', (req, res) => {
+  const { value } = req.body;
+
+  return res.status(200).json({ result: negate(value) });
+});
+
+router.post('/truthiness', (req, res) => {
+  const { value } = req.body;
+
+  return res.status(200).json({ result: truthiness(value) });
+});
+
+router.get('/is-odd/:a', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  return Number.isNaN(a)
+    ? res.status(400).json({ error: 'Parameter must be a number.' })
+    : res.status(200).json({ result: isOdd(a) });
+});
+
+router.get('/:a/starts-with/:b', (req, res) => {
+  const { a } = req.params;
+  const { b } = req.params;
+
+  if (startsWith(b, a)) {
+    res.status(200).json({ result: startsWith(b, a) });
+  }
+  if (b.length > 1) {
+    res.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  }
+  res.status(200).json({ result: false });
+});
 
 module.exports = router;
